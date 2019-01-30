69be2926dcb26cef9d8b68f0c41397972058818f
Nihal Sekhar <38371128+nihalsekhar@users.noreply.github.com>
2019-01-30 19:16:11 +0530

    Add files via upload

---
+++ MyProject/WebContent/home.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<div size="50%" align="center">
+<form action="Servlet">
+<input type = "text" name = "username" placeholder="username"> 
+<br>
+<br>
+<input type = "password" name = "password" placeholder="password">
+<br> 
+<br>
+<input type = "submit" value="submit">
+</form>
+</div>
+</body>
+</html>
---
+++ MyProject/src/com/cg/Servlet/Servlet.java
@@ -0,0 +1,60 @@
+package com.cg.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter printWriter = response.getWriter();
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		String userValid = "nihal";
+		String passValid = "nihal";
+		
+		if(username.equals(userValid)&&password.equals(passValid)) {
+		printWriter.println("Welcome Mr. "+username+password+LocalDate.now());
+	}
+		else 
+		{
+			request.getRequestDispatcher("home.jsp").include(request, response);
+			printWriter.println("Invalid");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
